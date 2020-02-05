2371c8856e8fcabf47efea56a2bde1dc3ef3707d
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-02-05 17:47:23 +0000

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (0);
 }
