92b93e1da5a3e26c2cec414c98ad31493dfbf31a
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-02-06 19:58:52 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main - main
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+printf("%d", x);
+printf("\n");
+return (0);
+}
