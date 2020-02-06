212c67c0ffcc1a3f5b0fb79f69f8ed3aa84f8612
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-02-06 20:21:43 +0000

    putchar_mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main - main
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char x;
+for (x = 'z'; x >= 'a'; --x)
+putchar(x);
+putchar('\n');
+return (0);
+}
