9922d77f90fdc3d48d4f8a11a862b2798179e292
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-02-06 20:11:59 +0000

    numbers_putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main - main
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+putchar(x + '0');
+putchar('\n');
+return (0);
+}
