a885bdfd527fd824db6912c052714bfb8762d193
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-02-06 18:56:00 +0000

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main - main
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; ++x)
+putchar(x);
+putchar('\n');
+return (0);
+}
