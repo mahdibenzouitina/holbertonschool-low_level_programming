a981e91088a9b1debc5ac540b2ae0fd0b9587313
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-02-07 22:23:59 +0000

    combinations_of_two_digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main - Entry point 
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+int b = 0;
+for (a = 0 ; a <= 89 ; a++)
+{
+b++;
+if (a < a % 10 * 10 + a / 10)
+{
+putchar (a / 10 + '0');
+putchar (a % 10 + '0');
+if (b < 89)
+{
+putchar (',');
+putchar (' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
