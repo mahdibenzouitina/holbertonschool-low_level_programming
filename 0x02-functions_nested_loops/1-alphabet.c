e16bcae9d8be30b2e519ee43ed3eddc357ab4361
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-02-10 13:30:09 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char n;
+for (n = 'a'; n <= 'z'; ++n)
+_putchar(n);
+_putchar('\n');
+}
