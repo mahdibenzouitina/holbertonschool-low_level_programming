43b704aa2177bada1a2ef78889be641a1504d690
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-02-10 11:20:50 +0000

    Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Return : 0
+ */
+int main(void)
+{
+char h[] = "Holberton";
+int i = 0;
+for (i = 0;  i <= 9; i++)
+_putchar(h[i]);
+_putchar('\n');
+return (0);
+}
