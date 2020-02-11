15feb6abbcb11c540da2e65a6a2b2efbf5c93e9a
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-02-11 12:38:01 +0000

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_last_digit - print last digit
+ * @i: variable
+ *
+ * Return: last digit
+ */
+int print_last_digit(int i)
+{
+int g = (i % 10);
+if (g < 0)
+g = i * -1;
+_putchar((g) + '0');
+return (i);
+}
