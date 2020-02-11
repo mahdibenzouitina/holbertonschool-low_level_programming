2716f6adb2ad29795995dfc42e156600df5a68af
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-02-11 21:23:59 +0000

    function that prints the 9 times table starting with 0

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * add - add two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
