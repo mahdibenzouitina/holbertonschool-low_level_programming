456f1569135b36339412d6496a3e7a23770c00fd
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-02-11 09:10:36 +0000

    print.

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
 char h[] = "Holberton";
 int i = 0;
-for (i = 0; i <= 9; i++)
+for (i = 0; i < 9; i++)
 _putchar(h[i]);
 _putchar('\n');
 return (0);
