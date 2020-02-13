1fd7803516e99f0f17f01f2c7988d986299147f7
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-02-13 21:16:34 +0000

    program that prints the numbers from 1 to 100 for mul 3 print Fizz for mul 5 print Buzz for mul 3 and 5 print FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Return: result
+ */
+int main(void)
+{
+int a;
+for (a = 1; a <= 100; a++)
+{
+if (a % 3 != 0 && a % 5 != 0)
+printf("%d", a);
+else
+if (a % 3 == 0 && a % 5 == 0)
+printf("FizzBuzz");
+else
+if (a % 5 == 0)
+printf("Buzz");
+else
+if (a % 3 == 0)
+printf("Fizz");
+if (a < 100)
+printf(" ");
+}
+printf("\n");
+return (0);
+}
