2536de15ef97c35b5c288f11acf8a5d52b6f4431
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-02-13 20:48:08 +0000

    function that prints a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_square - print square
+ * @size: variable
+ *
+ */
+void print_square(int size)
+{
+int a;
+int b;
+if (size > 0)
+{
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < size; b++)
+_putchar ('#');
+_putchar ('\n');
+}
+}
+else
+_putchar ('\n');
+}
