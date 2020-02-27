d8156bb190c61eacadae70479696c33d102da7a9
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-02-27 13:26:33 +0000

    int _pow_recursion(int x, int y)

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _pow_recursion - value of x raised to the power of y
+ * @x: number1
+ * @y: number2
+ *
+ * Return: vgsdh
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+return (1);
+else
+return (x * (_pow_recursion(x, y - 1)));
+}
