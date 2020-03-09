afc3f138e099018525d7e80bde6a6eb92392e5b0
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-03-09 22:08:54 +0000

    new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - dog id
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+typedef struct dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
