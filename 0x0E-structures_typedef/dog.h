90a5f0033aa44779f780444256505488ad9f8a1f
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-03-10 17:19:04 +0000

    updating dogh

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,6 +13,8 @@ char *name;
 float age;
 char *owner;
 } dog;
+
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
