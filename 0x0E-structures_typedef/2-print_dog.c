e476410afdc4f36c58e9e4369dbbd2c7289f4a7a
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-03-10 17:16:57 +0000

    fixing

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ if (d->name != '\0')
 printf("Name: %s\n", d->name);
 else
 printf("Name: (nil)\n");
-printf("Age: %.1f\n", d->age);
+printf("Age: %f\n", d->age);
 if (d->owner != '\0')
 printf("Owner: %s\n", d->owner);
 else
