c101ef532c45ba8ba9799f3115f552974013e737
mahdibenzouitina <ben.zouitina.mahdi97@gmail.com>
2020-03-31 22:01:45 +0000

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,12 +16,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 	i = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (i < 0)
 		return (-1);
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len] != '\0'; len++)
+			;
 			r = write(i, text_content, len);
 		if (r < 0)
 			return (-1);
